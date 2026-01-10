deed202c064d0f9c2460ee334061edb4503406fe
Ahmed_Eladawy <ahmed.ibrahim.eladawy7@gmail.com>
2026-01-10 16:51:09 +0200

    Prepare API & Client

--- client.py
+++ client.py
@@ -4,5 +4,9 @@ link = input("Enter URL: ")
 
 pack = {"url" : link}
 
-response = requests.post(url="https://127.0.0.1:8000", json=pack)
-print(response.content)
+response = requests.post(url="http://127.0.0.1:8000", json=pack)
+content = response.json()
+
+
+if "message" in content:
+    print(content["message"])
