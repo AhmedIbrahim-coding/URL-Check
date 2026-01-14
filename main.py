d60761e3afe9ffe77d071a7660abe3c0fb8d45f5
Ahmed_Eladawy <ahmed.ibrahim.eladawy7@gmail.com>
2026-01-14 14:48:00 +0200

    convert it to oriented (oop)

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from urllib.parse import urlparse, ParseResult
-import idna
-import ipaddress
-import re
+from normalize import Normalizer
 
 class Values(BaseModel):
     url: str
@@ -11,124 +8,15 @@ class Values(BaseModel):
 app = FastAPI()
 
 
-def normalize_url(url: str):
-    url = url.strip()
-    if not url:
-        return None
-    return url
-
-def ensure_scheme(url : str) -> str:
-    parsed = urlparse(url)
-
-    if "://" in url:
-        return url
-
-    if re.fullmatch(r"[a-zA-Z0-9.-]+:\d{1,5}", url):
-        return "https://" + url
-
-    if parsed.scheme:
-        return url
-    
-    return "https://" + url
-
-
-def validate_scheme(parsed: ParseResult) -> ParseResult:
-    allowed_schemes = {"http", "https"}
-    scheme = parsed.scheme.lower() if parsed.scheme else "https"
-
-    if scheme not in allowed_schemes:
-        raise Exception("Unsupported scheme")
-
-    return parsed._replace(scheme=scheme)
-
-def validate_hostname(parsed: ParseResult) -> ParseResult:
-    hostname = parsed.hostname
-    if not hostname:
-        raise Exception("Missing hostname")
-
-    hostname = hostname.lower().rstrip(".")
-
-    try:
-        ipaddress.ip_address(hostname)
-        normalized_host = hostname
-    except ValueError:
-        normalized_host = idna.encode(hostname).decode("ascii")
-
-    return parsed._replace(netloc=normalized_host)
-
-def normalize_port(parsed: ParseResult) -> ParseResult:
-    # store the port & the scheme in variables
-    port = parsed.port
-    scheme = parsed.scheme
-
-    # just continue if the port exists if it's not return immediatly 
-    if port == None:
-        return parsed
-
-    # raise an error if the port doesn't follow the port range rule
-    if not (1 <= port <= 65535):
-        raise Exception
-
-    # put default port numbers for both http & https
-    default_ports = {
-        "https" : 443,
-        "http" : 80,
-    }
-
-    # get the hostname, username(auth), and password(auth) form the url
-    hostname = parsed.hostname
-    username = parsed.username
-    password = parsed.password
-
-    # check if there is an (RFC 3986) authentication in the URL
-    auth = ""
-    if username:
-        auth = username
-        if password:
-            auth += f":{password}"
-        auth += "@"
-
-    # if the port is deafult to the scheme remove it is it not keep it
-    if port == default_ports.get(scheme):
-        netloc = f"{auth}{hostname}"
-    else:
-        netloc = f"{auth}{hostname}:{port}"
-
-    # replace the old netlock with the new built netloc
-    return parsed._replace(netloc=netloc)
-
-def remove_fragment(parsed : ParseResult) -> ParseResult:
-    frag = parsed.fragment
-    if not frag:
-        return parsed
-
-    return parsed._replace(fragment="")
-
 
 @app.post("/")
 def check_risk(values: Values):
-    url = normalize_url(values.url)
+
+    normalizer = Normalizer(values.url)
+
+    url = normalizer.normalize_url()
+
     if not url:
-        return {"message": "empty url"}
-
-    # ensure the scheme exists before parsing
-    url = ensure_scheme(url)
-
-    try:
-        parsed = urlparse(url)
-        parsed = validate_scheme(parsed)
-        parsed = validate_hostname(parsed)
-        parsed = normalize_port(parsed)
-        parsed = remove_fragment(parsed)
-
-    except Exception:
-        return {"message": "not safe"}
-
-    return {
-        "scheme": parsed.scheme,
-        "hostname": parsed.hostname,
-        "port": parsed.port,
-        "path": parsed.path,
-        "query": parsed.query,
-        "fragment" : parsed.fragment
-    }
+        return {"message" : "not safe"}
+    
+    return {"url" : url}
---
+++ normalize.py
@@ -0,0 +1,122 @@
+from urllib.parse import urlparse, ParseResult, urlunparse
+import idna
+import ipaddress
+import re
+
+
+class Normalizer:
+    def __init__(self, url):
+        self.url = url
+
+    
+    def normalize_url(self) -> str:
+        url = self.url.strip()
+        if not url:
+            return None
+        
+        # ensure the scheme exists before parsing
+        url = self.ensure_scheme(url)
+
+        try:
+            parsed = urlparse(url)
+            parsed = self.validate_scheme(parsed)
+            parsed = self.validate_hostname(parsed)
+            parsed = self.normalize_port(parsed)
+            parsed = self.remove_fragment(parsed)
+            url = urlunparse(parsed)
+        except Exception:
+            return None
+        
+        return url
+
+    def ensure_scheme(self, url : str) -> str:
+        parsed = urlparse(url)
+
+        if "://" in url:
+            return url
+
+        if re.fullmatch(r"[a-zA-Z0-9.-]+:\d{1,5}", url):
+            return "https://" + url
+
+        if parsed.scheme:
+            return url
+        
+        return "https://" + url
+    
+    
+    def validate_scheme(self, parsed: ParseResult) -> ParseResult:
+        allowed_schemes = {"http", "https"}
+        scheme = parsed.scheme.lower() if parsed.scheme else "https"
+
+        if scheme not in allowed_schemes:
+            raise Exception("Unsupported scheme")
+
+        return parsed._replace(scheme=scheme)
+
+
+    def validate_hostname(self, parsed: ParseResult) -> ParseResult:
+        hostname = parsed.hostname
+        if not hostname:
+            raise Exception("Missing hostname")
+
+        hostname = hostname.lower().rstrip(".")
+
+        try:
+            ipaddress.ip_address(hostname)
+            normalized_host = hostname
+        except ValueError:
+            normalized_host = idna.encode(hostname).decode("ascii")
+
+        return parsed._replace(netloc=normalized_host)
+
+    
+
+    def normalize_port(self, parsed: ParseResult) -> ParseResult:
+        # store the port & the scheme in variables
+        port = parsed.port
+        scheme = parsed.scheme
+
+        # just continue if the port exists if it's not return immediatly 
+        if port == None:
+            return parsed
+
+        # raise an error if the port doesn't follow the port range rule
+        if not (1 <= port <= 65535):
+            raise Exception
+
+        # put default port numbers for both http & https
+        default_ports = {
+            "https" : 443,
+            "http" : 80,
+        }
+
+        # get the hostname, username(auth), and password(auth) form the url
+        hostname = parsed.hostname
+        username = parsed.username
+        password = parsed.password
+
+        # check if there is an (RFC 3986) authentication in the URL
+        auth = ""
+        if username:
+            auth = username
+            if password:
+                auth += f":{password}"
+            auth += "@"
+
+        # if the port is deafult to the scheme remove it is it not keep it
+        if port == default_ports.get(scheme):
+            netloc = f"{auth}{hostname}"
+        else:
+            netloc = f"{auth}{hostname}:{port}"
+
+        # replace the old netlock with the new built netloc
+        return parsed._replace(netloc=netloc)
+    
+
+    def remove_fragment(self, parsed : ParseResult) -> ParseResult:
+        frag = parsed.fragment
+        if not frag:
+            return parsed
+
+        return parsed._replace(fragment="")
+
